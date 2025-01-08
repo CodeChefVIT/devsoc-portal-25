ecb6892584884db678f5d6ff32a227246909354c
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-01-09 02:38:49 +0530

    made settings page less ugly

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -72,74 +72,143 @@ export default function Settings() {
 
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <div className={"flex w-full flex-col gap-6"}>
-            <FormField
-              control={form.control}
-              name={"name"}
-              render={({ field }) => (
-                <AuthFormItem
-                  field={field}
-                  labelText={"Username"}
-                  type={"text"}
-                  required
-                  autoFill
+          <div className={"flex  w-full  border-2 border-black p-5 rounded-lg flex-col gap-6"}>
+            <div className="flex gap-10">
+              <div className="flex flex-col gap-6">
+                <FormField
+                  control={form.control}
+                  name={"name"}
+                  render={({ field }) => (
+                    <AuthFormItem
+                      field={field}
+                      labelText={"Username"}
+                      type={"text"}
+                      required
+                      autoFill
+                    />
+                  )}
                 />
-              )}
-            />
-            <FormField
-              control={form.control}
-              name={"email"}
-              render={({ field }) => (
-                <AuthFormItem
-                  field={field}
-                  labelText={"Email"}
-                  type={"text"}
-                  required
-                  autoFill
+                <FormField
+                  control={form.control}
+                  name={"email"}
+                  render={({ field }) => (
+                    <AuthFormItem
+                      field={field}
+                      labelText={"Email"}
+                      type={"text"}
+                      required
+                      autoFill
+                    />
+                  )}
                 />
-              )}
-            />
-            <FormField
-              control={form.control}
-              name={"college"}
-              render={({ field }) => (
-                <AuthFormItem
-                  field={field}
-                  labelText={"College"}
-                  type={"text"}
-                  required
-                  autoFill
+                <FormField
+                  control={form.control}
+                  name={"college"}
+                  render={({ field }) => (
+                    <AuthFormItem
+                      field={field}
+                      labelText={"College"}
+                      type={"text"}
+                      required
+                      autoFill
+                    />
+                  )}
                 />
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name={"reg_no"}
-              render={({ field }) => (
-                <AuthFormItem
-                  field={field}
-                  labelText={"Registration No."}
-                  type={"text"}
-                  required
-                  autoFill
+                <FormField
+                  control={form.control}
+                  name={"reg_no"}
+                  render={({ field }) => (
+                    <AuthFormItem
+                      field={field}
+                      labelText={"Registration No."}
+                      type={"text"}
+                      required
+                      autoFill
+                    />
+                  )}
                 />
-              )}
-            />
-            <FormField
-              control={form.control}
-              name={"phone_no"}
-              render={({ field }) => (
-                <AuthFormItem
-                  field={field}
-                  labelText={"Phone No."}
-                  type={"tel"}
-                  required
-                  autoFill
+                <FormField
+                  control={form.control}
+                  name={"phone_no"}
+                  render={({ field }) => (
+                    <AuthFormItem
+                      field={field}
+                      labelText={"Phone No."}
+                      type={"tel"}
+                      required
+                      autoFill
+                    />
+                  )}
                 />
-              )}
-            />
-
+              </div>
+              <div className="flex flex-col gap-6">
+                <FormField
+                  control={form.control}
+                  name={"phone_no"}
+                  render={({ field }) => (
+                    <AuthFormItem
+                      field={field}
+                      labelText={"Phone No."}
+                      type={"tel"}
+                      required
+                      autoFill
+                    />
+                  )}
+                />{" "}
+                <FormField
+                  control={form.control}
+                  name={"phone_no"}
+                  render={({ field }) => (
+                    <AuthFormItem
+                      field={field}
+                      labelText={"Phone No."}
+                      type={"tel"}
+                      required
+                      autoFill
+                    />
+                  )}
+                />{" "}
+                <FormField
+                  control={form.control}
+                  name={"phone_no"}
+                  render={({ field }) => (
+                    <AuthFormItem
+                      field={field}
+                      labelText={"Phone No."}
+                      type={"tel"}
+                      required
+                      autoFill
+                    />
+                  )}
+                />{" "}
+                <FormField
+                  control={form.control}
+                  name={"phone_no"}
+                  render={({ field }) => (
+                    <AuthFormItem
+                      field={field}
+                      labelText={"Phone No."}
+                      type={"tel"}
+                      required
+                      autoFill
+                    />
+                  )}
+                />{" "}
+                <FormField
+                  control={form.control}
+                  name={"phone_no"}
+                  render={({ field }) => (
+                    <AuthFormItem
+                      field={field}
+                      labelText={"Phone No."}
+                      type={"tel"}
+                      required
+                      autoFill
+                    />
+                  )}
+                />
+              </div>
+            </div>
             <CustomButton type="submit">UPDATE</CustomButton>
           </div>
         </form>
