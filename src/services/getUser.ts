6df74907425526ebd379772c4a577d21bbb38a36
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-01-09 00:55:19 +0530

    added fake data for testing

--- src/services/getUser.ts
+++ src/services/getUser.ts
@@ -1,10 +1,24 @@
-import { IUser } from "@/interfaces";
-import api from "."
+// import { IUser } from "@/interfaces";
+// import api from "."
 import {  throwAPIError } from "@/lib/error";
 
 export async function getUserDetails() {
     try {
-      const { data } = await api.get<IUser>(`/login/user`);
+      const data =  {
+        id: "23434",
+        name: "Abhinav",
+        team_id: "324",
+        email: "abhi@cc.com",
+        is_vitian: false,
+        reg_no: "213",
+        password: "213",
+        phone_no: "123",
+        role: "Super lender",
+        is_leader: false,
+        college: "afds",
+        is_verified: false,
+      }
+      // const { data } = await api.get<IUser>(`/me`);
       return data;
     } catch (e) {
       throw throwAPIError(e);
