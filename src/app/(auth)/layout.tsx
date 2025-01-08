6bcf071665799ed9b915c3ee9adc1630cd091ab9
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2025-01-08 23:59:59 +0530

    feat: signup, login, opt-verification

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/app/(auth)/_components/navbar";
+
+export default function AuthLayout({
+   children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className={"w-full h-full min-h-screen bg-[url('/images/auth-bg.svg')]"}>
+            <Navbar />
+            <div className={"w-full h-full pt-16 min-h-screen flex items-center justify-center"}>
+                {children}
+            </div>
+        </div>
+    );
+}
---
+++ src/app/(auth)/sign-up/(info)/team-formation/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const TeamFormation = () => {
+    return (
+        <div>TeamFormation</div>
+    )
+}
+export default TeamFormation
