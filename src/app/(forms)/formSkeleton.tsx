4049dcc9d14d37dfaa12d81aca2e8e8a890b47d5
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-01-11 02:27:10 +0530

    Reverted fixed width

--- src/app/(forms)/formSkeleton.tsx
+++ src/app/(forms)/formSkeleton.tsx
@@ -19,32 +19,30 @@ export default function FormSkeleton<T extends FieldValues>({
   onSubmit: (data: T) => void;
 }) {
   return (
-    <div>
       <div className="flex flex-col gap-5 items-center">
-        <h1 className="font-monomaniac text-2xl mt-14 mb-5">{title}</h1>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="flex w-full flex-col items-center gap-7"
-          >
-            <div className="flex rounded-lg flex-col gap-6">
-              <div className="flex border-4 bg-cc-plain p-10 pb-16 rounded-lg border-black gap-5">
-                <div className="flex w-[700px] flex-col gap-6">{children}</div>
+        <h1 className="font-monomaniac text-2xl mt-2 mb-5">{title}</h1>
+        <div className="flex w-[75%] border-4 bg-cc-plain p-10 pb-16 rounded-lg border-black gap-5">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="flex  flex-col items-center gap-7"
+            >
+              <div className="flex rounded-lg flex-col gap-6 items-center">
+                {children}
+                <div className="flex justify-center">
+                  <CustomButton
+                    type="submit"
+                    buttonProps={{
+                      className: "py-2 text-lg px-10",
+                    }}
+                  >
+                    {buttonText}
+                  </CustomButton>
+                </div>
               </div>
-              <div className="flex justify-center">
-                <CustomButton
-                  type="submit"
-                  buttonProps={{
-                    className: "py-2 text-lg px-10",
-                  }}
-                >
-                  {buttonText}
-                </CustomButton>
-              </div>
-            </div>
-          </form>
-        </Form>
+            </form>
+          </Form>
+        </div>
       </div>
-    </div>
   );
 }
