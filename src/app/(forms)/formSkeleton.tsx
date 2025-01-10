9f5519d6e2718e0c9f0ac48fa258ed2528f806e0
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-01-11 02:11:26 +0530

    fixed styling

--- src/app/(forms)/formSkeleton.tsx
+++ src/app/(forms)/formSkeleton.tsx
@@ -22,28 +22,28 @@ export default function FormSkeleton<T extends FieldValues>({
     <div>
       <div className="flex flex-col gap-5 items-center">
         <h1 className="font-monomaniac text-2xl mt-2 mb-5">{title}</h1>
-        <div className="flex border-4 bg-cc-plain p-10 pb-16 rounded-lg border-black gap-5">
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="flex w-full flex-col items-center gap-7"
-            >
-              <div className="flex rounded-lg flex-col gap-6">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex w-full flex-col items-center gap-7"
+          >
+            <div className="flex rounded-lg flex-col gap-6">
+              <div className="flex border-4 bg-cc-plain p-10 pb-16 rounded-lg border-black gap-5">
                 <div className="flex w-[700px] flex-col gap-6">{children}</div>
-                <div className="flex justify-center">
-                  <CustomButton
-                    type="submit"
-                    buttonProps={{
-                      className: "py-2 text-lg px-10",
-                    }}
-                  >
-                    {buttonText}
-                  </CustomButton>
-                </div>
               </div>
-            </form>
-          </Form>
-        </div>
+              <div className="flex justify-center">
+                <CustomButton
+                  type="submit"
+                  buttonProps={{
+                    className: "py-2 text-lg px-10",
+                  }}
+                >
+                  {buttonText}
+                </CustomButton>
+              </div>
+            </div>
+          </form>
+        </Form>
       </div>
     </div>
   );
