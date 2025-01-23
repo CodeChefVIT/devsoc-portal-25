06ec32f1d495a85595c0a1cad3f5f6247bfee2df
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-01-23 16:52:41 +0530

    fixed login getting scroll

--- src/app/(portal)/layout.tsx
+++ src/app/(portal)/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="h-screen">
+    <div >
       <Navbar></Navbar>
       {children}
     </div>
