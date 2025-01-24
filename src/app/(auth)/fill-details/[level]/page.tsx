f49fa404da2a462f28fc4d086f1a69bc025faaad
Abhinav Pant <67090539+abhitrueprogrammer@users.noreply.github.com>
2025-01-24 16:55:59 +0530

    Runtime in fill details

--- src/app/(auth)/fill-details/[level]/page.tsx
+++ src/app/(auth)/fill-details/[level]/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+export const runtime = 'edge';
 import React from 'react'
 import InfoWrapper from "@/app/(auth)/fill-details/_components/info-wrapper";
 import PersonalDetails from "@/app/(auth)/fill-details/_forms/personal-details";
