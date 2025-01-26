fe1e1b5dd6c93388b6a51e066a8542ed630c2f2c
Abh1noob <abhinavganeshank@gmail.com>
2025-01-27 01:45:36 +0530

    fix

--- src/app/(auth)/_components/modal.tsx
+++ src/app/(auth)/_components/modal.tsx
@@ -13,16 +13,16 @@ const Modal = ({ branding = true, text, classname, children }: IAuthModal) => {
       className={`w-fit px-10 py-14 bg-cc-plain border-4 border-cc-dark flex flex-col justify-center items-center rounded-2xl ${classname}`}
     >
       {/* TODO: Add Devsoc2k25 SVG */}
-      {/* {branding && (
+      {branding && (
         <div className={"flex font-yerk text-cc-primary relative select-none"}>
-          <div className={"text-6xl"}>DEVSOC</div>
+          {/* <div className={"text-6xl"}>DEVSOC</div>
           <div
             className={"rotate-90 text-xs absolute -right-3 inset-y-0 bottom-2"}
           >
             2K25
-          </div>
+          </div> */}
         </div>
-      )} */}
+      )}
       {text && (
         <div className={"font-monomaniac text-2xl mt-2 mb-3"}>{text}</div>
       )}
