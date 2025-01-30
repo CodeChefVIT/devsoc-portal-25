75d49aeed77f8df868d434a8daf2efed6359ff0e
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-01-30 23:16:05 +0530

    added password reset and resend otp routes

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -32,6 +32,11 @@ interface ISignupRequest {
   email: string;
   password: string;
 }
+interface IUpdatePasswordRequest {
+  email: string;
+  new_password: string;
+  otp: string;
+}
 interface IGithubProfile {
   github: string;
 }
@@ -43,7 +48,14 @@ export const signup = async (request: ISignupRequest) => {
     throw ConvertToAPIError(error);
   }
 };
-
+export const updatePassword = async (request: IUpdatePasswordRequest) => {
+  try {
+    const res = await api.post("/auth/update-password", request);
+    return getData(res.data);
+  } catch (error) {
+    throw ConvertToAPIError(error);
+  }
+};
 interface IVerifyOTPRequest {
   email: string;
   otp: string;
@@ -90,7 +102,14 @@ export const createTeam = async (request: ICreateTeamRequest) => {
     throw ConvertToAPIError(error);
   }
 };
-
+export const resendOTP = async (request: {email: string}) => {
+  try {
+    const res = await api.post("/auth/resend-otp", request);
+    return getData(res.data);
+  } catch (error) {
+    throw ConvertToAPIError(error);
+  }
+};
 interface IJoinTeamRequest {
   code: string;
 }
