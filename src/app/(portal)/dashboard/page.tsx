7a990ac0a7697d8e3d47757bb802eaefde7fddab
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-01-31 21:20:03 +0530

    added time in the last submission notification

--- src/app/(portal)/dashboard/page.tsx
+++ src/app/(portal)/dashboard/page.tsx
@@ -42,7 +42,7 @@ const Dashboard: React.FC = () => {
             role="alert"
           >
             <span className="block sm:inline">
-              Last day of submission is on 2nd!
+              Last day of submission is on 2nd February, 2PM!
             </span>
           </div>
         <div className="grid grid-cols-1 md:grid-cols-2  min-h-0 gap-4">
